0c7dd8ce4b2077aa85343cf550602dc73154918c
Tomáš Najnar <tomas.najnar@saholding.sa.priv>
2024-03-03 20:12:04 +0100

    TMVS: About my life text

--- src/components/AboutMe/AboutData.ts
+++ src/components/AboutMe/AboutData.ts
@@ -1,6 +1,6 @@
 export const aboutHeading: string = "Hello, I am Tomáš Najnar, frontend engineer and passionate web and mobile app developer with modern design. I like new things and I enjoy creating innovative projects. My focus is to create apps that are modern, contain interactive elements and are user-friendly.";
 
-export const aboutMyLife = "Outside of the digital world, I spend a lot of time at the gym and with friends. Exercise in the gym not only brings me joy, but also helps me maintain better discipline as a developer. I believe that discipline and determination are the keys to achieving excellence not only in exercise but also in the world.";
+export const aboutMyLife = "Outside of the digital world, I spend a lot of time with my family, friends and at the gym. Exercise in the gym not only brings me joy, but also helps me maintain better discipline as a developer. I believe that discipline and determination are the keys to achieving excellence not only in exercise but also in the world.";
 
 export const aboutTitle: string = "About me";
 
