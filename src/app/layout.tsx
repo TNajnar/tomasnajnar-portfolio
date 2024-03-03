ffe44ca8bd2cda3fe90b1e4d15247ae56c584566
Tomáš Najnar <tomas.najnar@saholding.sa.priv>
2024-03-03 19:51:05 +0100

    TMVS: Seo3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,19 @@ import { Footer, Header } from "@/components";
 import '@/styles/index.css';
 
 export const metadata: Metadata = {
-  title: "Tomáš Najnar",
   description: "Tomáš Najnar - frontend engineer specializing in the modern design and development of web and mobile applications.",
-  icons: "/tn-logo.svg"
+  icons: "/tn-logo.svg",
+  openGraph: {
+    description: "Tomáš Najnar - frontend engineer specializing in the modern design and development of web and mobile applications.",
+    siteName: "Tomáš Najnar",
+    title: "Tomáš Najnar",
+    type: "website",
+    url: "tomasnajnar.com",
+    images: [{
+      url: "tn-logo.svg",
+    }],
+  },
+  title: "Tomáš Najnar",
 };
 
 interface IProps {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 import { ReactElement } from "react";
 import { AboutMe, Contact, Introduction, Projects } from "@/components";
-import { NextSeo } from "next-seo";
 import clsx from "clsx";
 
 const Page = (): ReactElement => (
   <>
-    <NextSeo
-      title="Tomáš Najnar"
-      description="Tomáš Najnar - frontend engineer specializing in the modern design and development of web and mobile applications."
-    />
     <div className={clsx("page-layout", "flex flex-col gap-14 py-10 sm:gap-28 sm:py-24")}>
       <Introduction />
       {/* <Technologies /> */}
