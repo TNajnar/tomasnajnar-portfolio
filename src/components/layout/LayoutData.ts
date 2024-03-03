96508c8797793e5858de4d254d75eb96e5ebc76e
Tomáš Najnar <tomas.najnar@saholding.sa.priv>
2024-03-03 20:17:35 +0100

    TMVS: About me lower case

--- src/components/layout/LayoutData.ts
+++ src/components/layout/LayoutData.ts
@@ -3,7 +3,7 @@ import { EScrollTo } from "@/utils/enums";
 import { THeaderLink } from "@/utils/types";
 
 export const headerLinks: Array<THeaderLink> = [
-  { coordinates: COORDINATES, id: 88, path: EScrollTo.ABOUT_ME, title: 'About Me' },
+  { coordinates: COORDINATES, id: 88, path: EScrollTo.ABOUT_ME, title: 'About me' },
   { coordinates: COORDINATES, id: 89, path: EScrollTo.PROJECTS, title: 'Projects' },
   { coordinates: CONTACT_COORDINATES, id: 90, path: EScrollTo.CONTACT, title: 'Contact me' }
 ];
