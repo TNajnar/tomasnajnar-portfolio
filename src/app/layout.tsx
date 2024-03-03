5e4f4e8c83e68fc70e775c736cea0033ea799409
Tomáš Najnar <tomas.najnar@saholding.sa.priv>
2024-03-03 19:18:46 +0100

    TMVS: Seo2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next";
 import DarkThemeProvider from "./DarkThemeProvider";
 import { Footer, Header } from "@/components";
 import '@/styles/index.css';
-import { NextSeo } from "next-seo";
 
 export const metadata: Metadata = {
   title: "Tomáš Najnar",
@@ -18,10 +17,6 @@ interface IProps {
 const RootLayout = ({ children }: IProps): ReactElement => (
   <html lang="en" suppressHydrationWarning>
     <body>
-      <NextSeo
-        title="Tomáš Najnar"
-        description="Tomáš Najnar - frontend engineer specializing in the modern design and development of web and mobile applications."
-      />
       <DarkThemeProvider>
         <Header />
         <main className="mainComponent">
